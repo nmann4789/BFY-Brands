f37e683eea7bcbe8249b73e3542a37b13fb963f2
Nicholas Mann <nicholasmann@Nicholass-MacBook-Pro.local>
2016-12-20 19:29:06 -0500

    Content Update

--- header.php
+++ header.php
@@ -32,7 +32,6 @@
 			<div id="bfyNav">
 				<ul class="">
 					<li><a href="/">Home</a></li>
-					<li><a href="our-brands">Our Brands</a></li>
 					<li><a href="about-us">About Us</a></li>
 					<li><a href="our-team">Our Team</a></li>
 					<li><a href="careers">Careers</a></li>
